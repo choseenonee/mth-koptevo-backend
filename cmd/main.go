46bdd8ba01f2b55680c7b7656e8cc7ca63bf1646
choseenonee <okaymans44@gmail.com>
2024-04-01 19:51:52 +0300

    init main

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"mth/pkg/config"
+	"mth/pkg/database"
+	"mth/pkg/log"
+	"mth/pkg/trace"
+)
+
+const serviceName = "gin"
+
+func main() {
+	logger, loggerInfoFile, loggerErrorFile := log.InitLogger()
+	defer loggerInfoFile.Close()
+	defer loggerErrorFile.Close()
+
+	logger.Info("Logger Initialized")
+
+	config.InitConfig()
+	logger.Info("Config Initialized")
+
+	jaegerURL := fmt.Sprintf("http://%v:%v/api/traces", viper.GetString(config.JaegerHost), viper.GetString(config.JaegerPort))
+	tracer := trace.InitTracer(jaegerURL, serviceName)
+	logger.Info("Tracer Initialized")
+
+	db := database.GetDB()
+	logger.Info("Database Initialized")
+
+	_ = db
+	_ = tracer
+}
