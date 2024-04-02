9a541e6efc27d4eb64528fc1bf195e021a43093b
choseenonee <okaymans44@gmail.com>
2024-04-02 20:36:48 +0300

    add: place with filters test

--- internal/repository/place.go
+++ internal/repository/place.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/Masterminds/squirrel"
 	"github.com/jmoiron/sqlx"
 	"github.com/spf13/viper"
@@ -72,6 +74,38 @@ func (p placeRepo) Create(ctx context.Context, placeCreate models.PlaceCreate) (
 	return createdID, nil
 }
 
+func (p placeRepo) getPlaceTags(ctx context.Context, place *models.Place) error {
+	if place == nil {
+		return errors.New("you passing nil pointer to the getPlaceTags!")
+	}
+	query := `SELECT tags.id, name FROM tags
+				RIGHT JOIN places_tags pt on tags.id = pt.tag_id
+				WHERE pt.place_id = $1;`
+
+	rows, err := p.db.QueryContext(ctx, query, place.ID)
+	if err != nil {
+		return customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.ExecErr, Err: err})
+	}
+
+	for rows.Next() {
+		var tag models.Tag
+
+		err = rows.Scan(&tag.ID, &tag.Name)
+		if err != nil {
+			return customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.ScanErr, Err: err})
+		}
+
+		place.Tags = append(place.Tags, tag)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.RowsErr, Err: err})
+	}
+
+	return nil
+}
+
 // GetAllWithFilter todo: implement tagIDs and pagination
 func (p placeRepo) GetAllWithFilter(ctx context.Context, districtID int, cityID int, tagIDs []int, page int, name string) ([]models.Place, error) {
 	psql := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
@@ -83,7 +117,7 @@ func (p placeRepo) GetAllWithFilter(ctx context.Context, districtID int, cityID 
 			Join("places_tags ON places.id = places_tags.place_id").
 			Join("tags ON places_tags.tag_id = tags.id").
 			Where(squirrel.Eq{"places_tags.tag_id": tagIDs}).
-			GroupBy("places.id", "tags.id").
+			GroupBy("places.id").
 			Having("COUNT(DISTINCT places_tags.tag_id) >= ?", len(tagIDs))
 	}
 	if districtID != 0 {
@@ -120,9 +154,20 @@ func (p placeRepo) GetAllWithFilter(ctx context.Context, districtID int, cityID 
 			return []models.Place{}, customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.ScanErr, Err: err})
 		}
 
+		if len(places) > 0 {
+			if places[len(places)-1].ID == place.ID {
+				continue
+			}
+		}
+
 		err = json.Unmarshal(propertiesRaw, &place.Properties)
 		if err != nil {
 			return []models.Place{}, customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.BindErr, Err: err})
+		}
+
+		err = p.getPlaceTags(ctx, &place)
+		if err != nil {
+			return []models.Place{}, fmt.Errorf("get place tags, err: %v", err)
 		}
 
 		places = append(places, place)
