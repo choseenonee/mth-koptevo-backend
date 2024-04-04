6d516a88123aece7f1329c433acb6b1ca54fda10
choseenonee <okaymans44@gmail.com>
2024-04-05 02:12:36 +0300

    add: user repo

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/jmoiron/sqlx"
+	"mth/internal/models"
 	"mth/pkg/customerr"
 )
 
@@ -41,4 +43,51 @@ func (u userRepo) CheckInPlace(ctx context.Context, userID, placeID int) error {
 	}
 
 	return nil
+}
+
+func (u userRepo) GetUser(ctx context.Context, login string) (int, string, error) {
+	query := `SELECT id, password FROM users WHERE login = $1`
+
+	var userID int
+	var password string
+
+	err := u.db.QueryRowContext(ctx, query, login).Scan(&userID, &password)
+	if err != nil {
+		return 0, "", customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.ScanErr, Err: err})
+	}
+
+	return userID, password, nil
+}
+
+func (u userRepo) CreateUser(ctx context.Context, userCreate models.UserCreate) (int, error) {
+	propertiesRaw, err := json.Marshal(userCreate.Properties)
+	if err != nil {
+		return 0, customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.BindErr, Err: err})
+	}
+
+	tx, err := u.db.Beginx()
+	if err != nil {
+		return 0, customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.TransactionErr, Err: err})
+	}
+
+	createRouteQuery := `INSERT INTO users (login, password, properties) VALUES ($1, $2, $3) RETURNING id;`
+
+	var createdID int
+	err = tx.QueryRowxContext(ctx, createRouteQuery, userCreate.Login, userCreate.Password, propertiesRaw).Scan(&createdID)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return 0, customerr.ErrNormalizer(
+				customerr.ErrorPair{Message: customerr.ScanErr, Err: err},
+				customerr.ErrorPair{Message: customerr.RollbackErr, Err: rbErr},
+			)
+		}
+
+		return 0, customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.ScanErr, Err: err})
+	}
+
+	if err = tx.Commit(); err != nil {
+		return 0, customerr.ErrNormalizer(customerr.ErrorPair{Message: customerr.CommitErr, Err: err})
+	}
+
+	return createdID, nil
 }
