f67bffb17ff29112d61cb0ee23009093668d01f8
choseenonee <okaymans44@gmail.com>
2024-04-04 16:39:34 +0300

    upd: migr unique constraint

--- deploy/migrations/20240404120616_user_favourite_tables.sql
+++ deploy/migrations/20240404120616_user_favourite_tables.sql
@@ -1,15 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users_favourite_routes (
-  user_id INTEGER REFERENCES users(id),
-  route_id INTEGER REFERENCES routes(id),
-  timestamp TIMESTAMP
+    user_id INTEGER REFERENCES users(id),
+    route_id INTEGER REFERENCES routes(id),
+    timestamp TIMESTAMP,
+    UNIQUE (user_id, route_id)
 );
 
 CREATE TABLE IF NOT EXISTS users_favourite_places (
     user_id INTEGER REFERENCES users(id),
     place_id INTEGER REFERENCES places(id),
-    timestamp TIMESTAMP
+    timestamp TIMESTAMP,
+    UNIQUE (user_id, place_id)
 );
 -- +goose StatementEnd
 
